d00ccccb83c3aed46800c1a6737dc2eb3a553d63
sansdev <sansdev.malang@gmail.com>
2026-08-04 14:02:18 +0700

    Clean up unused test files and temporary scripts

--- test_api.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-$response = Illuminate\Support\Facades\Http::get('http://sans-hrd.test/api/attendance-matrix', ['month' => '2026-07', 'unit_id' => 'sd']);
-echo json_encode(array_slice($response->json()['data'] ?? [], 0, 1), JSON_PRETTY_PRINT);
