2bc397ca1cb082c15f1a2e1b3f376d61d630f8f7
Muhamad Ihwan <ihwan.2705@gmail.com>
2026-09-13 12:28:46 +0700

    fix(dashboard): update route references from legacy siswa/rombel to students.index/classrooms.index

--- routes/web.php
+++ routes/web.php
@@ -33,13 +33,13 @@ Route::middleware(['auth', 'verified', 'role:super_admin,admin_sd,admin_paud,adm
     // Classrooms (Rombongan Belajar)
     Route::get('/classrooms/{id}/students', [ClassroomController::class, 'students'])->name('classrooms.students');
     Route::resource('classrooms', ClassroomController::class);
-    Route::get('/rombel', fn() => redirect()->route('classrooms.index'));
+    Route::get('/rombel', fn() => redirect()->route('classrooms.index'))->name('rombel');
 
     // Students (Data Siswa & Bulk Excel Import)
     Route::get('students/download-template', [StudentController::class, 'downloadTemplate'])->name('students.download-template');
     Route::post('students/import', [StudentController::class, 'import'])->name('students.import');
     Route::resource('students', StudentController::class);
-    Route::get('/siswa', fn() => redirect()->route('students.index'));
+    Route::get('/siswa', fn() => redirect()->route('students.index'))->name('siswa');
 });
 
 
